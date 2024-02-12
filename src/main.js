d7bb975019e36b601637fc238a36b1aa128cecda
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2024-02-12 16:56:44 +1100

    JS - Header, Home, Arrow

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY > headerHeight) {
+        header.classList.add('header--dark')
+    } else {
+        header.classList.remove('header--dark')
+    }
+});
+
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+
+document.addEventListener('scroll', () => {
+    // console.log(1 - window.scrollY / homeHeight);
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+const arrowUp = document.querySelector('.arrow-up');
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight/2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+});
