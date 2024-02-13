3ba5d86256e41d279fa7cf7457cab4c7e9d027fa
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2024-02-13 16:31:49 +1100

    JS - Projects Filtering

--- src/projects.js
+++ src/projects.js
@@ -1,2 +1,36 @@
 'use strict';
 
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+
+categories.addEventListener('click', (event) => {
+    const filter = event.target.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    handleActiveSelction (event.target);
+    filteringProjects (filter);
+});
+
+function handleActiveSelction (target) {
+    const active = document.querySelector('.category--selected');
+    active.classList.remove('category--selected');
+    target.classList.add('category--selected');
+}
+
+function filteringProjects (filter) {
+    projectsContainer.classList.add('anim-out');
+    
+    setTimeout(() => {
+        projectsContainer.classList.remove('anim-out');
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter === 'all' || filter === project.dataset.type) {
+                project.style.display = 'block';
+            } else {
+                project.style.display = 'none';
+            }
+        });
+    }, 250);
+}
