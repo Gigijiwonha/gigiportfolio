73983c0a87d871f82c7a583bc386779df5ca2b33
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2024-02-15 21:39:47 +1100

    Style update

--- src/type.js
+++ src/type.js
@@ -1,5 +1,16 @@
 'use strict';
 
-new TypeIt('.home__title--strong')
-.move(-10)
+new TypeIt('.home__title--strong', {
+    loop : true,
+    speed :100,
+})
+.move(-11)
+.delete(6)
+.type('Technology')
+.move(11)
+.delete(10)
+.type('Lover')
 .go();
+
+
+// Design Enthusiast and Technology Lover
